92d21cf56efe038d9aa9d6b04b75b80733ed652c
D@reK@r@n18 <xpresskaran98@gmail.com>
2023-11-30 15:32:37 +0530

    changed ui for test dashboard

--- client/src/pages/TestDashboard.jsx
+++ client/src/pages/TestDashboard.jsx
@@ -14,75 +14,79 @@ const TestDashboard = ({ TestName }) => {
   };
 
   return (
-    <div className="">
-      <div className="">
-        <div className="flex items-center justify-center w-1/5 m-auto">
-          <img src={logo} alt="" />
-        </div>
-        {/* user name */}
-        <div className=" mt-5">
-          <select
-            name="userDashboard"
-            className="text-center bg-gray-100 w-1/4"
-            id=""
-          >
-            {" "}
-            <option value="anish" selected>
-              Anish
-            </option>
-            <option value="anishwanare9@gmail.com" disabled>
-              Email : anishwanare9@gmail.com
-            </option>
-            <option value="logout">Logout</option>
-          </select>
-        </div>
-        <hr className="h-1 bg-black" />
-      </div>
-      <div className="text-xs mt-10 p-2 flex flex-col gap-2">
-        <p className="text-green-600 font-semibold">
-          Dnyanankur Publication {" > "} Test Center {" > "} Mock Test {" > "} Test
-          Details
-        </p>
-        <p className="font-bold text-base">Test Preview</p>
-        <div className="bg-white p-8 rounded shadow-md max-w-md text-base">
-          <ul>
-            <li className="mb-2">
-              <span className="font-bold">Test Name:</span> {TestName}
-            </li>
-            <li className="mb-2">
-              <span className="font-bold">Subject:</span> English
-            </li>
-            <li className="mb-2">
-              <span className="font-bold">Total Questions:</span> 15
-            </li>
-            <li className="mb-2">
-              <span className="font-bold">Total Marks:</span> 15
-            </li>
-            <li className="mb-2">
-              <span className="font-bold">Negative Marking:</span> No negative
-              marking
-            </li>
-            <li className="mb-2">
-              <span className="font-bold">Time available:</span> 30 min
-            </li>
-          </ul>
-          <div className="flex gap-3 justify-end my-5">
-            <button
-              type="button"
-              className="bg-blue-400 p-1 text-lg border-b-black border shadow-md"
-              onClick={handleOnStart}
+    <div className="bg-gray-900 text-white min-h-screen">
+      <div className="container mx-auto p-4 sm:p-8">
+        <div className="flex flex-col items-center justify-center">
+          <img
+            src={logo}
+            alt="Logo"
+            className="w-2/3 sm:w-1/3 md:w-1/4 lg:w-1/5 m-auto"
+          />
+
+          <div className="mt-4">
+            <select
+              name="userDashboard"
+              className="text-center bg-gray-800 text-white w-full p-2 rounded"
             >
-              Start Exam
-            </button>
-            <button
-              type="button"
-              className="p-1 text-lg bg-gray-200 border-b-black border shadow-md"
-            >
-              <Link to={"/test-dashboard"}>Back</Link>
-            </button>
+              <option value="anish">Anish</option>
+              <option value="anishwanare9@gmail.com" disabled>
+                Email: anishwanare9@gmail.com
+              </option>
+              <option value="logout">Logout</option>
+            </select>
           </div>
-          <div className="text-gray-500 text-sm">
-            <p>Thanks for Appearing Test</p>
+          <hr className="h-1 bg-white w-full my-4" />
+        </div>
+
+        <div className="text-sm mt-6 p-4 flex flex-col gap-4">
+          <p className="text-green-400 font-semibold">
+            Dnyanankur Publication {" > "} Test Center {" > "} Mock Test {" > "}{" "}
+            Test Details
+          </p>
+          <p className="font-bold text-lg">Test Preview</p>
+
+          <div className="bg-gray-800 p-4 rounded shadow-md">
+            <ul className="list-disc pl-4">
+              <li>
+                <span className="font-bold">Test Name:</span> {TestName}
+              </li>
+              <li>
+                <span className="font-bold">Subject:</span> English
+              </li>
+              <li>
+                <span className="font-bold">Total Questions:</span> 15
+              </li>
+              <li>
+                <span className="font-bold">Total Marks:</span> 15
+              </li>
+              <li>
+                <span className="font-bold">Negative Marking:</span> No negative
+                marking
+              </li>
+              <li>
+                <span className="font-bold">Time available:</span> 30 min
+              </li>
+            </ul>
+
+            <div className="flex flex-col sm:flex-row justify-center sm:justify-end mt-6">
+              <button
+                type="button"
+                className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded mb-2 sm:mb-0 sm:mr-4"
+                onClick={handleOnStart}
+              >
+                Start Exam
+              </button>
+              <button
+                type="button"
+                className="bg-gray-600 hover:bg-gray-700 text-white py-2 px-4 rounded"
+              >
+                <Link to="/test-dashboard">Back</Link>
+              </button>
+            </div>
+
+            <div className="text-gray-300 text-sm mt-4">
+              <p>Thanks for Appearing Test</p>
+            </div>
           </div>
         </div>
       </div>
