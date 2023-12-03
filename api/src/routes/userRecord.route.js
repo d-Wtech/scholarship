dc579c819e1446c1c605f8d5442879baf916b83d
D@reK@r@n18 <xpresskaran98@gmail.com>
2023-12-03 16:24:34 +0530

    created get user result route

--- api/src/routes/userRecord.route.js
+++ api/src/routes/userRecord.route.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { recordResponses } from "../controllers/userRecord.controller.js";
+import {
+  recordResponses,
+  sendResult,
+} from "../controllers/userRecord.controller.js";
 import { authenticateUser } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/:testName/submit-test", authenticateUser, recordResponses);
+router.get("/:testName/get-result", authenticateUser, sendResult);
 
 export default router;
