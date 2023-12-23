128e315f4c904fe4752ab7d5ad6b5cc887f8d6c9
Anish Wanare <106481793+Anishwanare@users.noreply.github.com>
2023-12-23 19:13:53 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 9090;
 // setting up cors
 app.use(
   cors({
-    origin: process.env.ORIGIN, // specify the allowed origin
+    origin: api.dnyanankur.in , dnyanankur.in , // specify the allowed origin
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     optionsSuccessStatus: 204,
